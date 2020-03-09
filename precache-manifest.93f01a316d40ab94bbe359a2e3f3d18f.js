de925d2d508f9c171aa13ff5f09a7e03fe55158a
Nakul Tanta <nakul.tanta@grazitti.local>
2020-03-09 18:32:16 +0530

    Updates

---
+++ precache-manifest.93f01a316d40ab94bbe359a2e3f3d18f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda5c060323da9003664e5021daf4290",
+    "url": "/reacttodo/index.html"
+  },
+  {
+    "revision": "36b057a0f92181e9ce56",
+    "url": "/reacttodo/static/css/main.74550cb5.chunk.css"
+  },
+  {
+    "revision": "df967976d83c8a7ea440",
+    "url": "/reacttodo/static/js/2.807260c1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reacttodo/static/js/2.807260c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b057a0f92181e9ce56",
+    "url": "/reacttodo/static/js/main.189d5a48.chunk.js"
+  },
+  {
+    "revision": "de217e6ad6b7c44dec92",
+    "url": "/reacttodo/static/js/runtime-main.d72425fe.js"
+  }
+]);
